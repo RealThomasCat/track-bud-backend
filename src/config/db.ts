3c6311098047838391b1177cea9c636061027c53
RealThomasCat <yashveer.rtc@gmail.com>
2026-05-17 16:43:44 +0530

    feat(db): configure PrismaClient logging for dev environment

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+    // In development log prisma queries, errors and warnings, in production log only errors
+    log:
+        process.env.NODE_ENV === "development"
+            ? ["query", "error", "warn"]
+            : ["error"],
+});
